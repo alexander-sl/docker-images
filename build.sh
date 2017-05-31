3d58e9ba428f0b9fc482e50daf4442aebd77038b
Alexander Slitskiy <aslitsky@larch-networks.com>
2017-05-31 16:53:25 +0300

    Add and check deps

--- build.sh
+++ build.sh
@@ -14,32 +14,47 @@ build_image() {
     dockerfile_path=$1
     dockerfile_dir=`dirname ${dockerfile_path}`
     docker_image="${IMAGE_PREFIX}/${dockerfile_dir}"
+    if ! [ -z $2 ]; then
+        base_image_name=`dirname $2`
+        base_image_name=${IMAGE_PREFIX}/${base_image_name}
+        base_image_name=`echo $base_image_name | sed 's=/=\\\/=g'`
+        `sed -e "s/##base_image/${base_image_name}/g" $dockerfile_path > $dockerfile_dir/Dockerfile`
+        dockerfile_path=$dockerfile_dir/Dockerfile
+    fi
 
-    echo "Building $docker_image..."
+    echo "Building $docker_image $base_image..."
 
-    #docker build --build-arg DOCKER_GROUP=${DOCKER_GROUP} --build-arg USERID=${USERID} --build-arg GROUPID=${GROUPID} -f ${dockerfile_path} -t ${docker_image}:${TAG} ${dockerfile_dir}
+    docker build --build-arg DOCKER_GROUP=${DOCKER_GROUP} --build-arg USERID=${USERID} --build-arg GROUPID=${GROUPID} -f ${dockerfile_path} -t ${docker_image}:${TAG} ${dockerfile_dir}
 }
 
 build_dep() {
     local dockerfile_path=$1
+    local base_image=$2
     local dockerfile_dir=`dirname ${dockerfile_path}`
+    local deps_suffix=`echo ${dockerfile_dir} | sed 's/\//_/g'`
+    local deps_var="DEPS_${deps_suffix}"
     local docker_image="${IMAGE_PREFIX}/${dockerfile_dir}"
     echo "Processing $dockerfile_path image $docker_image..."
 
     if [ -f $dockerfile_path ]; then
         # Dockerfile exists
+        # Check image exists...
         image_built=`docker images | grep $docker_image | awk '{ print $1 }'`
         if [ -z $image_built ]; then
+
+
             echo "No built image $docker_image found."
+
             if [ -f $dockerfile_dir/deps ]; then
+                #Image has dependencies
                 echo "Image $docker_image has dependencies"
                 source $dockerfile_dir/deps
-                for dep in ${DEPS[*]}
-                do
-                    echo "Processing dependency $dep for $docker_image"
-                    build_dep $dep
-                done
-                build_image $dockerfile_path
+                local deps=${!deps_var}
+                if ! [ -z $deps ]; then
+                    echo "Processing dependency $deps for $docker_image"
+                    build_dep $deps
+                fi
+                build_image $dockerfile_path $deps
             else
                 build_image $dockerfile_path
             fi
@@ -80,5 +95,4 @@ if [ -f $DOCKERFILE_PATH/deps ]; then
 
 fi
 build_dep $DOCKERFILE_PATH
-#docker build --build-arg DOCKER_GROUP=${DOCKER_GROUP} --build-arg USERID=${USERID} --build-arg GROUPID=${GROUPID} -f ${DOCKERFILE_PATH} -t ${DOCKER_IMAGE}:${TAG} ${DOCKERFILE_DIR}
 
